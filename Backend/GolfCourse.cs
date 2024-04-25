5d6bab8662ea00d0de1e28d98c30a953c0f49575
NickCampbell97 <njc1749@gmail.com>
2024-04-24 19:48:03 -0500

    modified:   Backend/GolfCourse.cs

--- Backend/GolfCourse.cs
+++ Backend/GolfCourse.cs
@@ -2,10 +2,10 @@ namespace Backend;
 
 public class GolfCourse
 {
-    public string Name { get; set; } = null!;
-    public string StreetAddress { get; set; } = null!;
-    public string City  { get; set; } = null!;
-    public string State  { get; set; } = null!;
-    public string ZipCode  { get; set; } = null!;
-    public string PhoneNumber  { get; set; } = null!;
+    public string? Name { get; set; }
+    public string? StreetAddress { get; set; }
+    public string? City  { get; set; }
+    public string? State  { get; set; }
+    public string? ZipCode  { get; set; }
+    public string? PhoneNumber  { get; set; }
 }
