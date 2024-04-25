7d70b17f417e38651306723321de5920c23fb1c1
NickCampbell97 <njc1749@gmail.com>
2024-04-24 19:45:56 -0500

    new file:   Backend/GolfCourse.cs

---
+++ Backend/GolfCourse.cs
@@ -0,0 +1,11 @@
+namespace Backend;
+
+public class GolfCourse
+{
+    public string Name { get; set; } = null!;
+    public string StreetAddress { get; set; } = null!;
+    public string City  { get; set; } = null!;
+    public string State  { get; set; } = null!;
+    public string ZipCode  { get; set; } = null!;
+    public string PhoneNumber  { get; set; } = null!;
+}
